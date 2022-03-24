2c223e64c665595e47362503df997e06e8d4f33d
JulianIBM <63408537+JulianIBM@users.noreply.github.com>
2022-03-24 03:33:27 +0100

    feat: Implement all requirements

--- mars_rover/js/src/rover.js
+++ mars_rover/js/src/rover.js
@@ -1,18 +1,110 @@
 class Rover {
-  constructor(x, y, orientation) {
+  constructor(x, y, orientation, obsX = -1, obsY = -1 ) {
     this.x = x
     this.y = y
     this.orientation = orientation
+    this.obsX = obsX
+    this.obsY = obsY
   }
 
   move(movs) {
+    let obstacle = false;
+    
     movs.forEach(m => {
+      if(obstacle === true) return;
       if (m === "f") {
-        this.y = this.y + 1
+        switch (this.orientation) {
+          case 'N':
+            if (obstacle = this.detectObstacle(this.x, ((this.y + 1) % 3 + 3) % 3)) {
+              return; 
+            }
+            this.y += 1;
+            this.y = (this.y % 3 + 3) % 3;
+            break;
+          
+          case 'E':
+            if (obstacle = this.detectObstacle(((this.x + 1) % 3 + 3) % 3, this.y)) {
+              return; 
+            }
+            this.x += 1;
+            this.x = (this.x % 3 + 3) % 3;
+            break;
+          
+          case 'S':
+            if (obstacle = this.detectObstacle(this.x, ((this.y - 1) % 3 + 3) % 3)) {
+              return; 
+            }
+            this.y -= 1;
+            this.y = (this.y % 3 + 3) % 3;
+            break;
+          
+          case 'W':
+            if (obstacle = this.detectObstacle(((this.x - 1) % 3 + 3) % 3, this.y)) {
+              return; 
+            }
+            this.x -= 1;
+            this.x = (this.x % 3 + 3) % 3;
+            break;
+        }
       } else if (m === "b") {
-        this.y = this.y - 1
+        switch (this.orientation) {
+          case 'N':
+            if (obstacle = this.detectObstacle(this.x, ((this.y - 1) % 3 + 3) % 3)) {
+              return; 
+            }
+            this.y -= 1
+            this.y = (this.y % 3 + 3) % 3;
+            break;
+          
+          case 'E':
+            if (obstacle = this.detectObstacle(((this.x - 1) % 3 + 3) % 3, this.y)) {
+              return; 
+            }
+            this.x -= 1;
+            this.x = (this.x % 3 + 3) % 3;
+            break;
+          
+          case 'S':
+            if (obstacle = this.detectObstacle(this.x,  ((this.y + 1) % 3 + 3) % 3)) 
+            {
+              return; 
+            }
+            this.y += 1;
+            this.y = (this.y % 3 + 3) % 3;
+            break;
+          
+          case 'W':
+            if (obstacle = this.detectObstacle(((this.x + 1) % 3 + 3) % 3, this.y)) {
+              return; 
+            }
+            this.x += 1;
+            this.x = (this.x % 3 + 3) % 3;
+            break;
+        }
+        
+      } else if (m === "l" || m === "r") {
+        this.turn(m);
       }
     })
+  }
+
+  turn(direction) {
+    const dir = ['N', 'E', 'S', 'W'];
+
+    if(direction === "l") {
+      this.orientation = dir[(dir.indexOf(this.orientation) % dir.length + dir.length - 1) % dir.length];
+    } else {
+      this.orientation = dir[(dir.indexOf(this.orientation) % dir.length + dir.length + 1) % dir.length];
+    }
+  }
+
+  detectObstacle(ox, oy) {
+    if (ox === this.obsX && oy === this.obsY) {
+      console.log("Obstacle in X = " + ox + ", Y = " + oy);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
